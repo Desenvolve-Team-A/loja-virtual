3820237fc36e0e5f27ff47ebdb7e68e423224b65
Richard Nathan Domingos <richard.nathan@philips.com>
2024-06-05 13:54:58 -0300

    Produtos.js

---
+++ produtos.js
@@ -0,0 +1,314 @@
+let camisetas = [
+    {
+        "id": 1,
+        "título": "Camiseta Oversized Estampada Annuit - Verde Floresta",
+        "descricao": "Nossa camiseta em meia malha é composta pelo fio 30/1 penteado - 100% algodão. Possui um toque muito macio e confortável, com uma durabilidade incrível. Acompanhamos todo processo desde a compra do fio, que é cuidadosamente escolhido pelos dados do cultivo de algodão até o ínicio da produção na nossa fábrica em Blumenau. Prezamos muito pela qualidade, longevidade e responsabilidade ambiental em cada camiseta e é por isso que designamos nossa produção de matéria-prima nas melhores indústrias têxteis do Brasil.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 89.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Verde"
+    },
+    {
+        "id": 2,
+        "título": "Camiseta Oversized Estampada - Butterfly Marrom",
+        "descricao": "Apresentando a camisa que transpira estilo de rua. Feita de algodão premium para conforto e durabilidade, é perfeita para um dia com os amigos ou para curtir seus lugares favoritos. Combine com jeans rasgados e tênis de cano alto para um visual streat.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 89.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Marrom"
+    },
+    {
+        "id": 3,
+        "título": "Camiseta Regular Estampada – Basica Preta",
+        "descricao": "Expresse seu lado artístico com nossa camisa. Inspirada na energia das ruas, esta camisa combina designs sofisticados com um toque moderno. Feita com tecido macio e respirável, mantém você fresco e estiloso o dia todo. Seja explorando a cidade ou relaxando com seus amigos, esta camisa com certeza vai chamar atenção. Use com calças cargo e tênis marcantes para um visual descolado.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 69.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Preta"
+    },
+    {
+        "id": 4,
+        "título": "Camiseta Oversized Estampada – Lobo Roxa",
+        "descricao": "Entre nas ruas iluminadas por néon com nossa camisa. Eletrize seu guarda-roupa com suas cores vibrantes e gráficos inspirados na vida urbana. Feita com materiais de alta qualidade, oferece estilo e conforto para suas aventuras urbanas. Seja para curtir uma balada ou uma festa no terraço, esta camisa é a peça perfeita para se destacar. Combine com jeans skinny preto e botas robustas para um visual digno das ruas.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 89.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Roxa"
+    },
+    {
+        "id": 5,
+        "título": "Camiseta Regular Estampada – Lighter Marrom",
+        "descricao": "Atualize seu estilo urbano com nossa camisa Lighter. Com uma interpretação contemporânea das clássicas camisas basicas, esta camisa exala sofisticação urbana. Feita com tecido de algodão premium, oferece um caimento relaxado para conforto o dia todo. Os detalhes sofisticados adicionam interesse visual, tornando-a uma peça versátil para qualquer ocasião casual. Vista com calças de alfaiataria e mocassins para um visual streetwear refinado que com certeza impressionará.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 79.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Marrom"
+    },
+    {
+        "id": 6,
+        "título": "Camiseta Oversized Estampada – Alive Areia",
+        "descricao": "Atualize seu estilo urbano com nossa camisa Alive. Esta camisa é perfeita para passeio um sofisticado, seja explorando a cidade ou relaxando com seus amigos. Feita com tecido de algodão 30/1 penteado, oferece um caimento relaxado para conforto o dia todo.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 89.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Bege"
+    }
+]
+
+let calcas = [
+    {
+        "id": 1,
+        "título": "Calça Sarja Regular – Cinza",
+        "descricao": "Faça uma declaração ousada com nossas calças Denim Urbano Desgastado. Inspiradas nas ruas da cidade, essas calças combinam o estilo clássico do denim com um toque moderno de desgaste. Com um corte relaxado e detalhes estratégicos, elas proporcionam um visual urbano autêntico. Perfeitas para qualquer aventura urbana, estas calças se destacam quando combinadas com uma camiseta básica e um par de botas robustas.",
+        "tamanho": {
+            "38": 2,
+            "40": 4,
+            "42": 4,
+            "44": 2
+        },
+        "preco": 189.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "NA",
+        "tecido": "Algodão",
+        "cor": "Cinza"
+    },
+    {
+        "id": 2,
+        "título": "Calça Sarja Cargo – Areia",
+        "descricao": "Domine as ruas com nossas calças sarja cargo. Projetadas para o explorador urbano moderno, essas calças combinam funcionalidade com estilo. Equipadas com bolsos utilitários e detalhes de costura contrastantes, elas oferecem um visual urbano robusto. Feitas de tecido resistente e elástico, proporcionam conforto e liberdade de movimento durante todo o dia. Ideal para qualquer aventura na cidade, essas calças ficam perfeitas com uma jaqueta bomber e tênis de cano alto.",
+        "tamanho": {
+            "38": 2,
+            "40": 4,
+            "42": 4,
+            "44": 2
+        },
+        "preco": 229.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "NA",
+        "tecido": "Algodão",
+        "cor": "Bege"
+    },
+    {
+        "id": 3,
+        "título": "Calça de Moletom Elastic – Preta",
+        "descricao": "Adicione uma dose de estilo streetwear ao seu guarda-roupa com nossas calças Urbanas. Com um corte moderno e detalhes de bainha cortada, estas calças são a escolha perfeita para um visual descontraído e descolado. Feitas de algodãoe respirável, são ideais para os dias mais quentes da cidade. Combine com uma camiseta gráfica e tênis de estilo retrô para um look urbano casual e cheio de personalidade.",
+        "tamanho": {
+            "38": 2,
+            "40": 4,
+            "42": 4,
+            "44": 2
+        },
+        "preco": 169.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "NA",
+        "tecido": "Algodão",
+        "cor": "Preta"
+    },
+    {
+        "id": 4,
+        "título": "Calça Cargo Elastic – Off White",
+        "descricao": "Domine as ruas com nossa linha de Calças Cargo. Estas calças combinam funcionalidade e estilo. Equipadas com bolsos espaçosos e detalhes de fechamento com botões, são perfeitas para transportar seus itens essenciais com facilidade. Feitas de tecido resistente e durável, garantem conforto e liberdade de movimento durante todo o dia. Combine com uma jaqueta bomber e botas de combate para um visual urbano e robusto.",
+        "tamanho": {
+            "38": 2,
+            "40": 4,
+            "42": 4,
+            "44": 2
+        },
+        "preco": 229.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "NA",
+        "tecido": "Algodão",
+        "cor": "Off White"
+    },
+    {
+        "id": 5,
+        "título": "Calça de Moletom Elastic – Areia",
+        "descricao": "Adicione uma dose de estilo streetwear ao seu guarda-roupa com nossas calças Urbanas. Com um corte moderno e detalhes de bainha cortada, estas calças são a escolha perfeita para um visual descontraído e descolado. Feitas de algodãoe respirável, são ideais para os dias mais quentes da cidade. Combine com uma camiseta gráfica e tênis de estilo retrô para um look urbano casual e cheio de personalidade.",
+        "tamanho": {
+            "38": 2,
+            "40": 4,
+            "42": 4,
+            "44": 2
+        },
+        "preco": 169.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "NA",
+        "tecido": "Algodão",
+        "cor": "Bege"
+    },
+    {
+        "id": 6,
+        "título": "Calça de Moletom Elastic – Preta",
+        "descricao": "Esta calça personifica a essência do streetwear com um toque contemporâneo e uma vibe urbana inconfundível. Feita para aqueles que vivem nas ruas como sua própria passarela, esta peça é uma fusão de estilo e conforto, pronta para enfrentar qualquer movimento da cidade sem comprometer o visual. Com um corte moderno e detalhes cuidadosamente pensados, esta calça é mais do que apenas uma peça de vestuário; é uma declaração de moda. Feita de materiais de alta qualidade, ela abraça o corpo de forma elegante, sem sacrificar a liberdade de movimento.",
+        "tamanho": {
+            "38": 2,
+            "40": 4,
+            "42": 4,
+            "44": 2
+        },
+        "preco": 169.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "NA",
+        "tecido": "Algodão",
+        "cor": "Preta"
+    }
+]
+
+let moletons = [
+    {
+        "id": 1,
+        "título": "Moletom Canguru Capuz – ML Areia",
+        "descricao": "Redescubra o estilo das ruas com nosso Moletom Canguru ML. Inspirado nos clássicos do passado, este moletom combina nostalgia com uma vibração moderna. Feito de algodão macio e escovado para o máximo conforto, é perfeito para dias relaxantes na cidade. Com um design simples e atemporal, é fácil de combinar com qualquer look streetwear. Use com jeans e tênis para um visual casual e descolado.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 229.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "NA",
+        "tecido": "Algodão",
+        "cor": "Bege"
+    },
+    {
+        "id": 2,
+        "título": "Blusão Moletom – Butterfly Marrom",
+        "descricao": "O design incorpora elementos distintos do streetwear, como estampas gráficas clássicas, detalhes  com uma vibe descontraída, mas sofisticada. A paleta de cores é versátil, permitindo combinações infinitas com outras peças do guarda-roupa streetwear. Este blusão é mais do que apenas uma peça de roupa, é uma declaração de estilo e uma expressão de autenticidade. Seja para uma sessão de skate com os amigos, uma tarde relaxante na cidade ou simplesmente para mostrar sua personalidade única, este moletom é a escolha perfeita para quem busca conforto sem sacrificar o estilo urbano.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 189.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Marrom"
+    },
+    {
+        "id": 3,
+        "título": "Moletom Canguru Capuz – Butterfly Preta",
+        "descricao": "Faça uma declaração ousada com nosso Moletom Butterfly. Apresentando estampas vibrantes e gráficos audaciosos, este moletom é uma obra de arte em si. Feito de tecido macio e quente, é ideal para os dias mais frios da cidade. Com um corte moderno e descontraído, é perfeito para adicionar um toque de estilo urbano ao seu guarda-roupa. Use com calças cargo e botas para um visual streetwear autêntico.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 22.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Preta"
+    },
+    {
+        "id": 4,
+        "título": "Moletom Canguru Capuz – Fun Rosa",
+        "descricao": "Adicione um toque de estilo urbano ao seu visual com nosso Moletom Fun Rosa. Com detalhes de corte a laser e acabamentos, este moletom é uma atualização moderna do clássico básico. Feito de tecido macio e confortável, oferece um ajuste relaxado e descontraído. Perfeito para camadas durante os meses mais frios ou como peça principal em dias mais amenos, este moletom é versátil e estiloso. Use com jeans skinny e botas para um look streetwear descolado.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 229.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Rosa"
+    },
+    {
+        "id": 5,
+        "título": "Moletom Canguru Capuz – China Branco",
+        "descricao": "Apresentando um design colorido e vibrante, este moletom é uma homenagem ao estilo urbano. Feito de tecido macio e quente, é perfeito para adicionar um toque de nostalgia ao seu guarda-roupa. Com um ajuste descontraído e detalhes de painel de cor contrastante, é uma peça essencial para qualquer coleção streetwear. Use com calças skynny e tênis chunky para um visual autêntico.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 299.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Branca"
+    },
+    {
+        "id": 6,
+        "título": "Moletom Canguru Capuz – Glasses Roxo",
+        "descricao": "Este moletom é o epítome do streetwear contemporâneo, unindo conforto e estilo em uma peça única. Feito para os que vivem e respiram a energia das ruas, este moletom é mais do que uma simples roupa; é uma afirmação de identidade e um reflexo da cultura urbana. Com um corte moderno e detalhes meticulosamente pensados, este moletom destaca-se pela sua estética única. Feito com tecidos de alta qualidade, proporciona um toque suave e aconchegante, ideal para enfrentar os desafios do dia a dia na cidade.",
+        "tamanho": {
+            "P": 2,
+            "M": 4,
+            "G": 4,
+            "GG": 2
+        },
+        "preco": 229.99,
+        "categoria": 1,
+        "composicao": "100% algodão",
+        "estampa": "Lisa",
+        "tecido": "Algodão",
+        "cor": "Roxa"
+    }
+]
