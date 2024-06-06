305c85a9833da884ac390e84344447207a790e1d
Richard Nathan Domingos <richard.nathan@philips.com>
2024-06-06 11:07:11 -0300

    Formas de Pagamento - Hidden

---
+++ formulario.js
@@ -0,0 +1,23 @@
+function radioSelecionado(radio){
+
+    let selectedValue = radio.value;
+
+    const contents = document.querySelectorAll('.pagamento');
+
+    if(selectedValue == "pix"){
+        contents[0].classList.remove('hidden');
+        contents[1].classList.add('hidden');
+        contents[2].classList.add('hidden');
+
+    } else if (selectedValue == "cartao"){
+        contents[0].classList.add('hidden');
+        contents[1].classList.remove('hidden');
+        contents[2].classList.add('hidden');
+
+    } else if (selectedValue == "boleto"){
+        contents[0].classList.add('hidden');
+        contents[1].classList.add('hidden');
+        contents[2].classList.remove('hidden');
+    }
+
+}
